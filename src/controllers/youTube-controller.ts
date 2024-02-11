1418210bd1fc972be7e3c44ee487876916ace0c3
HelicopterHelicopter <jheel787@gmail.com>
2024-02-11 20:08:40 +0530

    changes video query param from url to videoId

--- src/controllers/youTube-controller.ts
+++ src/controllers/youTube-controller.ts
@@ -22,14 +22,14 @@ export const searchYT = async (req:Request,res:Response,next:NextFunction) => {
 
 export const ytStreamer = async(req:Request, res:Response,next:NextFunction) => {
     try{
-        const videoId = req.query.url as string;
+        const videoId = req.query.videoId as string;
         console.log(videoId);
         //const info  = await ytdl.getInfo(videoId);
         
         res.setHeader('Content-Type',"video/mp4");
         res.setHeader('Accept-Ranges',"bytes");
 
-        ytdl(videoId).pipe(res);
+        ytdl(`https://www.youtube.com/watch?v=${videoId}`).pipe(res);
     }catch(e){
         console.log('Error',e);
         res.status(500).json({message:"ERROR",cause:e.message});
