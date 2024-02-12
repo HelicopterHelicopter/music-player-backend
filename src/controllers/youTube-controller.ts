9bbcc7c0695fa63aa0c46cd4d0f1af35bebaf483
c209362 <jheel.rathod@iifl.com>
2024-02-12 11:50:32 +0530

    added presigned yt api testing

--- src/controllers/youTube-controller.ts
+++ src/controllers/youTube-controller.ts
@@ -2,6 +2,9 @@
 import { NextFunction,Request,Response } from "express";
 import { GetYTSearchResults } from "../utils/youtube-api-communicator";
 import ytdl from "ytdl-core";
+import { S3Client } from "@aws-sdk/client-s3";
+const fs = require('fs');
+
 export const searchYT = async (req:Request,res:Response,next:NextFunction) => {
     try{
         const searchKeyword = req.query.searchKeyword as string;
@@ -9,7 +12,7 @@ export const searchYT = async (req:Request,res:Response,next:NextFunction) => {
 
         if(apiResponse.items && apiResponse.items.length>0){
             const data = apiResponse.items.map((item)=>({
-                url:"https://www.youtube.com/watch?v=oMZHqoZojZ0"+item.id.videoId,
+                url:"https://www.youtube.com/watch?v="+item.id.videoId,
                 title:item.snippet.title,
                 thumbnail:item.snippet.thumbnails.default.url
             }));
@@ -33,5 +36,28 @@ export const ytStreamer = async(req:Request, res:Response,next:NextFunction) => 
     }catch(e){
         console.log('Error',e);
         res.status(500).json({message:"ERROR",cause:e.message});
+    }
+}
+
+export const getYtPresignedUrl = async(req:Request,res:Response) => {
+    try{
+        const videoId = req.query.videoId as String;
+
+        ytdl(`https://www.youtube.com/watch?v=${videoId}`).pipe(fs.createWriteStream(`${videoId}.mp4`));
+
+        const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+        const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+        const region = process.env.S3_REGION;
+        const Bucket = process.env.S3_BUCKET;
+
+        const client = new S3Client({
+            credentials:{
+                accessKeyId,
+                secretAccessKey
+            },
+            region
+        });
+    }catch(e){
+        console.log(e);
     }
 }
--- src/routes/yt-routes.ts
+++ src/routes/yt-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { searchYT, ytStreamer } from "../controllers/youTube-controller";
+import { getYtPresignedUrl, searchYT, ytStreamer } from "../controllers/youTube-controller";
 
 
 const yt_routes = Router();
 
 yt_routes.get("/search",searchYT);
 yt_routes.get("/play",ytStreamer);
+yt_routes.get("/getYtURL",getYtPresignedUrl);
 
 export default yt_routes;
